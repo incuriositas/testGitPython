554c1af78fc1c6d8f28d95a1414cc941ccffe1db
Suye0n <incuriositas@naver.com>
2020-06-19 21:52:01 +0900

    data update 2020619

--- test.py
+++ test.py
@@ -1,10 +1,25 @@
-from git import Repo
+# from git import Repo
 import time
 now = time.gmtime(time.time())
 y, m, d  = now.tm_year, now.tm_mon, now.tm_mday 
 msg  = "data update "+str(y)+str(m)+str(d)
-repo = Repo("./")
-repo.git.add(update=True)
-repo.index.commit(msg)
-origin = repo.remote(name="master")
-origin.push("--set-upstream")
+# repo = Repo("./")
+# repo.git.add(update=True)
+# repo.index.commit(msg)
+# origin = repo.remote(name="origin")
+# branch = repo.head()
+# origin.push("--set-upstream", origin.ma)
+
+#-*- coding: utf-8 -*-
+import sys
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+
+import subprocess
+import os
+
+os.chdir('./')
+
+subprocess.call(["git", "add", "."])
+subprocess.call(["git", "commit", "-m", msg])
+subprocess.call(["git", "push", "-u", "origin", "master"])
